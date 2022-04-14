493533453721c6fb9cbf065f498f0aba3cefe3b9
MaximoTrejo <maximotrejo123@gmail.com>
2022-04-14 12:56:34 -0300

    1er TP

---
+++ Tp-Uno/src/Tp-Uno.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : Tp-Uno.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "biblioteca.h"
+
+
+int main(void) {
+	setbuf (stdout, NULL);
+	menuInicio();
+	return EXIT_SUCCESS;
+}
